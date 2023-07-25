ad100936d59eabd4cf4be2c88af8c69b2e83e91a
Juadeb <juadebgabriel@gmail.com>
2023-07-25 14:10:24 +0100

    finishing up the dashboard page

--- dashboard.js
+++ dashboard.js
@@ -1,2 +1,31 @@
-// Dashboard Header Profile DropDown
-const toggle_profile = document.getElementById("toggle_profile");
+document.addEventListener("DOMContentLoaded", () => {
+  const navItems = document.querySelectorAll(".nav-item");
+
+  function setActiveNavItem() {
+    const currentPage = window.location.pathname.split("/").pop();
+    navItems.forEach((navItem) => {
+      const href = navItem
+        .querySelector("a")
+        .getAttribute("href")
+        .split("/")
+        .pop();
+      if (currentPage === href) {
+        navItem.classList.add("active");
+      } else {
+        navItem.classList.remove("active");
+      }
+    });
+  }
+
+  setActiveNavItem();
+
+  navItems.forEach((navItem) => {
+    navItem.addEventListener("click", () => {
+      navItems.forEach((item) => item.classList.remove("active"));
+
+      navItem.classList.add("active");
+
+      localStorage.setItem("lastClickedItemIndex", index);
+    });
+  });
+});
